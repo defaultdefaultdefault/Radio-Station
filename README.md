4326b1eabdca416a015a61c2d79bb327f2bdda11
cinareray <eraycinar@outlook.com>
2022-08-25 22:01:32 +0300

    radio_extrem_Add

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Proje clone edildikten sonra kendi branc'inize gecin.
 
 development_eray  ve development_mutlu
+
+https://stackoverflow.com/questions/55479574/android-mediaplayer-java-io-ioexception-prepare-failed-status-0x1
+https://www.youtube.com/watch?v=Z4DQdeMAJRE
--- app/src/main/java/com/radioz/radioz/MainActivity.java
+++ app/src/main/java/com/radioz/radioz/MainActivity.java
@@ -3,22 +3,52 @@ package com.radioz.radioz;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.AudioAttributes;
+import android.media.AudioManager;
 import android.media.Image;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageButton facebook, whatsapp, youtube;
+
+    Button button;
+    MediaPlayer mediaPlayer;
+    Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mediaPlayer = new MediaPlayer();
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              if(mediaPlayer.isPlaying()){
+                  handler.removeCallbacks(updater);
+                  mediaPlayer.pause();
+                  button.setText("play");
+              }
+              else {
+                  mediaPlayer.start();
+                  button.setText("pause");
+                }
+            }
+
+
+        });
+
+        preparedMediaPlayer();
 
         facebook = (ImageButton) findViewById(R.id.facebook_button);
         facebook.setOnClickListener(new View.OnClickListener() {
@@ -49,5 +79,50 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void preparedMediaPlayer(){
+
+        /*mediaPlayer.setAudioAttributes(new AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                .build());*/
+        try {
+            mediaPlayer.setDataSource(this, Uri.parse("http://199.180.75.118:80/stream"));
+            mediaPlayer.prepare();
+            button.setText(mediaPlayer.getDuration());
+        }catch (Exception exception){
+            Toast.makeText(this, exception.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private Runnable updater = new Runnable() {
+        @Override
+        public void run() {
+            long currentDuration = mediaPlayer.getCurrentPosition();
+
+        }
+    };
+
+    private String milliSecondsToTimer(long milliSeconds){
+        String timerString = "";
+        String secondString;
+
+        int hours = (int)(milliSeconds / (1000*60*60));
+        int minutes = (int) (milliSeconds % (1000 *60*60)) / (1000*60);
+        int seconds = (int) ((milliSeconds % (1000 * 60* 60)) % (1000*60) / 1000);
+
+        if(hours > 0){
+            timerString = hours + ":";
+        }
+
+        if(seconds < 10){
+            secondString = "0" + seconds;
+        }else {
+            secondString = "" + seconds;
+        }
+
+        timerString = timerString + minutes + ":" + secondString;
+        return timerString;
     }
 }
